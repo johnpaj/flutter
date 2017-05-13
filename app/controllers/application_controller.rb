43befc7e102292ac87917b92e3519f71f816f99d
John Jao <patricksjao@yahoo.com>
2017-05-13 08:32:32 +0000

    Added username column, user profile pages, and username sign-up form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # Lets not load this up if we're not accessing through a devise controller because if we access it using every controller we're wasting resources
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  
+  # protect the database, while allowing these fields to be updated
+  protected
+  
+  def configure_permitted_parameters
+    # On the sign up page you should be able to pass these variables back
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :email, :password, :password_confirmation, :remember_me) }
+    # We remove :username from the account update page because we dont want people updating their usernames
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :remember_me) }
+  end
+  
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,25 @@
 class PagesController < ApplicationController
+  # back end code for pages/index 
   def index
   end
-
+  
+  # back end code for pages/home
   def home
   end
-
+  
+  # back end code for pages/profile
   def profile
+    # grab the username from the URL as :id
+    # the if statement will check the database if there is a username e.g. if there is no "joeman" in the db, it will redirect
+    if (User.find_by_username (params[:id]))
+      @username = params[:id]
+    else
+      # redirect to 404 (root for now)
+      redirect_to root_path, :notice=> "User not found"
+    end
   end
-
+  
+  # back end code for pages/explore
   def explore
   end
 end
