9501057d6f9a76910cdc6c12a77c50f05e02fae4
John Jao <patricksjao@yahoo.com>
2017-05-16 15:04:23 +0000

    Aligned posts so that when more than one post is created it doesnt fuck up the grid. Fixed Home Profile and Explore so that Profile will display only that users posts and that explore will display all posts. Home will be fixed to display the posts of those you follow only.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,9 @@ class PagesController < ApplicationController
   
   # back end code for pages/home
   def home
+    @posts = Post.all
   end
-  
+  # this is the page that corresponds to ALL the pages.
   # back end code for pages/profile
   def profile
     # grab the username from the URL as :id
@@ -17,9 +18,13 @@ class PagesController < ApplicationController
       # redirect to 404 (root for now)
       redirect_to root_path, :notice=> "User not found"
     end
+    
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    #.where("user_id = ?", User.find_by_username (params[:id]).id)
   end
   
   # back end code for pages/explore
   def explore
+    @posts = Post.all
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,5 @@ class Post < ActiveRecord::Base
   # This will make sure that there is something written in the post and that it is of length <=140 characters --> ERROR if > 140
   validates :content, presence: true, length: { maximum: 140 }
   # Newest tweets / posts first --> sorts by date of post created
-  default_scope -> { order(created_at :desc) }
+  default_scope -> { order(created_at: :desc) }
 end
